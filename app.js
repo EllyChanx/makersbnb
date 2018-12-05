e6b7793d18772e6140bda75b49ee12dad3b4206c
leoncross <leon@lscross.co.uk>
2018-12-05 18:14:32 +0000

    added default available for property create

--- app.js
+++ app.js
@@ -88,11 +88,11 @@ app.post('/property_created', function(req, res) {
     price: req.body.price,
     description: req.body.description,
     date1: req.body.date1,
-    date1Status: req.body.date1Status,
+    date1Status: 'Available',
     date2: req.body.date2,
-    date2Status: req.body.dateStatus,
+    date2Status: 'Available',
     date3: req.body.date3,
-    date3Status: req.body.date3Status
+    date3Status: 'Available'
   }
   db.properties.insert(newProperty, function(req, result){
     res.redirect('/properties');
